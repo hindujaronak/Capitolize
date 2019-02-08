54259748a0b3b5592ed8c707b2839cb8e489f068
Bhavika Shahani <2015bhavika.shahani@ves.ac.in>
2019-02-08 19:05:24 +0530

    Login Finalized
    front end of login finalized

--- client/src/components/login/LoginView.js
+++ client/src/components/login/LoginView.js
@@ -29,100 +29,61 @@ const UserAccountDetails = ({ title }) => (
           <Col>
             <Form>
               <Row form>
-                {/* First Name */}
+               
+                {/* Email */}
                 <Col md="6" className="form-group">
-                  <label htmlFor="feFirstName">First Name</label>
-                  <FormInput
-                    id="feFirstName"
-                    placeholder="First Name"
-                    value="FirstName"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* Last Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feLastName">Last Name</label>
+                  <label htmlFor="feLastName">Email id</label>
                 <FormGroup>
                     <InputGroup className="mb-3">
                         <InputGroupAddon type="prepend">
                             <InputGroupText></InputGroupText>
                         </InputGroupAddon>
-                        <FormInput placeholder="Last Name" />
+                        <FormInput placeholder="Email id" />
                     </InputGroup>
                 </FormGroup>
                 </Col>
               </Row>
               <Row form>
-                {/* Email */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feEmail">Email</label>
-                  <FormInput
-                    type="email"
-                    id="feEmail"
-                    placeholder="Email Address"
-                    value="FirstName@example.com"
-                    onChange={() => {}}
-                    autoComplete="email"
-                  />
-                </Col>
+                
                 {/* Password */}
                 <Col md="6" className="form-group">
                   <label htmlFor="fePassword">Password</label>
-                  <FormInput
-                    type="password"
-                    id="fePassword"
-                    placeholder="Password"
-                    value="EX@MPL#P@$$w0RD"
-                    onChange={() => {}}
-                    autoComplete="current-password"
-                  />
+                  
+                  <FormGroup>
+                   <InputGroup className="mb-3">
+                        <InputGroupAddon type="prepend">
+                            <InputGroupText></InputGroupText>
+                        </InputGroupAddon>
+                        <FormInput type="password"
+                        id="fePassword"
+                        placeholder="Password"
+                         />
+                    </InputGroup>
+                </FormGroup>
                 </Col>
               </Row>
-              <FormGroup>
-                <label htmlFor="feAddress">Address</label>
-                <FormInput
-                  id="feAddress"
-                  placeholder="Address"
-                  value="1234 Main St."
-                  onChange={() => {}}
-                />
-              </FormGroup>
-              <Row form>
-                {/* City */}
+              
+                <Row>
+                {/* Confirm Password */}
                 <Col md="6" className="form-group">
-                  <label htmlFor="feCity">City</label>
-                  <FormInput
-                    id="feCity"
-                    placeholder="City"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* State */}
-                <Col md="4" className="form-group">
-                  <label htmlFor="feInputState">State</label>
-                  <FormSelect id="feInputState">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </FormSelect>
-                </Col>
-                {/* Zip Code */}
-                <Col md="2" className="form-group">
-                  <label htmlFor="feZipCode">Zip</label>
-                  <FormInput
-                    id="feZipCode"
-                    placeholder="Zip"
-                    onChange={() => {}}
-                  />
+                  <label htmlFor="fePassword">Confirm Password</label>
+                  
+                  <FormGroup>
+                   <InputGroup className="mb-3">
+                        <InputGroupAddon type="prepend">
+                            <InputGroupText></InputGroupText>
+                        </InputGroupAddon>
+                        <FormInput type="password"
+                        id="fePassword"
+                        placeholder="Confirm Password"
+                         />
+                    </InputGroup>
+                </FormGroup>
                 </Col>
               </Row>
-              <Row form>
-                {/* Description */}
-                <Col md="12" className="form-group">
-                  <label htmlFor="feDescription">Description</label>
-                  <FormTextarea id="feDescription" rows="5" />
-                </Col>
-              </Row>
-              <Button theme="accent">Update Account</Button>
+              
+              
+              <Button theme="accent">Login</Button>
             </Form>
           </Col>
         </Row>
@@ -139,7 +100,7 @@ UserAccountDetails.propTypes = {
 };
 
 UserAccountDetails.defaultProps = {
-  title: "Register Details"
+  title: "Login Details"
 };
 
 export default UserAccountDetails;
