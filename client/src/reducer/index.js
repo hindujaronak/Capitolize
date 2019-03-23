fbeb4422f18da7bf6748d477ef21b7e57ed51f10
Bhavika Shahani <2015bhavika.shahani@ves.ac.in>
2019-03-23 17:33:18 +0530

    links removed

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -4,8 +4,3 @@ import { itemReducer } from './itrmReducer';
 export default combineReducers({
     item : itemReducer
 })
-
-
-https://appdividend.com/2018/05/30/react-axios-tutorial-example-from-scratch/
-
-youtube: https://www.youtube.com/watch?v=s1swJLYxLAA
