a81ea4480d553dc8177c845bdd3695309f1815c4
Unknown <murlidharsharma69@gmail.com>
2019-03-27 13:22:27 +0530

    Front end changes

--- client/src/components/add-new-post/SidebarCategories.js
+++ client/src/components/add-new-post/SidebarCategories.js
@@ -1,5 +1,4 @@
-import React , {Component} from "react";
-// import React from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {
   Card,
@@ -14,6 +13,7 @@ import {
   FormCheckbox
   
 } from "shards-react";
+
 
 const SidebarCategories = ({ title }) => (
   <Card small className="mb-3">
@@ -85,8 +85,10 @@ const SidebarCategories = ({ title }) => (
           <FormCheckbox className="mb-1" value="Other sectors" >
             Other sectors
           </FormCheckbox>
+          
         </ListGroupItem>
-
+        <br></br>
+        <br></br>
         <ListGroupItem className="d-flex px-3">
           <InputGroup className="ml-auto">
             <FormInput placeholder="New category" />
@@ -100,7 +102,7 @@ const SidebarCategories = ({ title }) => (
       </ListGroup>
     </CardBody>
   </Card>
-);  
+);
 
 SidebarCategories.propTypes = {
   /**
